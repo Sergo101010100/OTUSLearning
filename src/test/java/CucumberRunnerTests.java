b3999bafe831c322fc2ca4be4f6cdd40a79dfbfe
Александр Поляков <poljakovag@mail.ru>
2024-03-30 23:08:24 +0300

    cucumber 7

--- src/test/java/CucumberRunnerTests.java
+++ src/test/java/CucumberRunnerTests.java
@@ -1,5 +1,22 @@
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.*;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("ru.otus.qa.auto")
+@ConfigurationParameters({
+        @ConfigurationParameter(
+                key = GLUE_PROPERTY_NAME,
+                value = "ru.otus.qa.auto"
+        ),
+        @ConfigurationParameter(
+                key = PLUGIN_PROPERTY_NAME,
+                value = "org.jetbrains.plugins.cucumber.java.run.CucumberJvm5SMFormatter"
+        ),
+        @ConfigurationParameter(
+                key = FEATURES_PROPERTY_NAME,
+                value = "src/test/resources/ru.otus.qa.auto")
+})
 public class CucumberRunnerTests {
 }
